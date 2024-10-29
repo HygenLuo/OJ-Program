7d17180a760b2c6bce8bbd120589fd8b58a49add
Hygen <hygenluo@gmail.com>
2024-10-30 00:16:23 +0800

    add ojfrontend

---
+++ ojfrontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="#app">
+    <BasicLayout />
+  </div>
+  <nav>
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<style>
+#app {
+}
+</style>
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { onMounted } from "vue";
+
+/**
+ * 全局初始化函数， 有全局单次调用的代码都可以写到这里
+ */
+
+const doInit = () => {
+  console.log("Welcome!");
+};
+
+onMounted(() => {
+  doInit();
+});
+</script>
---
+++ ojfrontend/src/views/NoAuthView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>你没有权限</div>
+</template>
+
+<style scoped></style>
